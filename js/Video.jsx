d1a9836fd27a1e1f7b9885c066f3ffa410b2a721
Luis_Pascal <pascalbarrossuarez@gmail.com>
2023-03-07 16:18:08 +0000

    modal hecho

--- js/Video.jsx
+++ js/Video.jsx
@@ -2,8 +2,8 @@ class Videos extends HTMLElement {
   constructor() {
     super();
     this.innerHTML = `<iframe 
-      width="560" 
-      height="315" 
+      width="100%" 
+      height="100%" 
       src="https://www.youtube.com/embed/NU9NYU9O0Hw" 
       title="YouTube video player" 
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
--- js/index.js
+++ js/index.js
@@ -1,12 +1,4 @@
-const miModal = document.querySelector(".miModal");
-const netflix = document.getElementById("netflix");
-const audio = document.getElementById("netflixSound");
-
-function playSound(sound, video = false) {
+function playSound(sound) {
     const mySound = document.getElementById(sound);
     mySound.play();
-
-    if (video) {
-      miModal.setAttribute("aria-hidden", "false"); 
-    }
 }
